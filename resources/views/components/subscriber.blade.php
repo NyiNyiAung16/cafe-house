5aa81e21592599b601ce54283f86c1a878973fdc
Nyi Nyi Aung <nyinyi31088@gmail.com>
2023-11-23 19:06:41 +0630

    created subscriber feature

--- resources/views/components/subscriber.blade.php
+++ resources/views/components/subscriber.blade.php
@@ -1,3 +1,15 @@
-<div>
-    subscriber
+<div class="subscriberContainer">
+    <div class="lg:max-w-3xl mx-auto">
+        <div class="mb-5 px-5">
+            <h3 class="text-center text-2xl text-white mb-3">Subscribe For new products</h3>
+            <p class="text-center text-white">If you subscribe, you will get a email when we will post a new product. Also subscribers can get discount three months a day. So get subscribe quickly as fast!!</p>
+        </div>
+        <form action="" class="subscriberForm">
+            <div class="form-group">
+                <input type="email" class="input" placeholder="Email">
+                <label for="email" class="label text-yellow-400">Email</label>
+            </div>
+            <button class="px-3 py-2 mt-4 bg-gray-700 rounded text-white mx-auto block">Subscribe</button>
+        </form>
+    </div>
 </div>
